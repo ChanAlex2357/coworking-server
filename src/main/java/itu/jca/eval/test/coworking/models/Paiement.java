5bb67b095ed633f014e877b461397453d7583475
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-12 00:22:24 +0300

    encapsulation de paiement

--- src/main/java/itu/jca/eval/test/coworking/models/Paiement.java
+++ src/main/java/itu/jca/eval/test/coworking/models/Paiement.java
@@ -1,26 +1,43 @@
 package itu.jca.eval.test.coworking.models;
 
-import java.time.LocalDate;
+import java.util.Date;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
-
-@Data
+import lombok.Getter;
+import itu.jca.eval.test.coworking.utils.TimeUtils;
+@Getter
 @Entity
 @Table(name = "paiement")
 public class Paiement {
     @Id
     private String id;
 
-    @Column(name = "datePaiement", nullable = false)
-    private LocalDate datePaiement;
+    @Column(name = "datepaiement", nullable = false)
+    private Date datePaiement;
 
     @ManyToOne
-    @JoinColumn(name = "idReservation", nullable = false)
+    @JoinColumn(name = "idreservation", nullable = false)
     private Reservation reservation;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public void setDatePaiement(Date datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public void setDatePaiement(String datePaiement) {
+        setDatePaiement(TimeUtils.formatDate(datePaiement));
+    }
 } 
