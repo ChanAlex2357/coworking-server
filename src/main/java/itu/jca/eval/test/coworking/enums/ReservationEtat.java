9946feff23725e600b90f91205d39c987189e4db
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-16 08:16:12 +0300

    enumeration des etats

---
+++ src/main/java/itu/jca/eval/test/coworking/enums/ReservationEtat.java
@@ -0,0 +1,16 @@
+package itu.jca.eval.test.coworking.enums;
+
+public enum ReservationEtat {
+    RESERVER(10),
+    VALIDER(11),
+    PAYER(12),
+    VALIDER_PAIEMENT(13);
+    
+    private final int etat;
+    private ReservationEtat(int etat){
+        this.etat = etat;
+    }
+    public int getEtat() {
+        return etat;
+    }
+}
